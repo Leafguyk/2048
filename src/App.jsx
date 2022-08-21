83bca517e69b442cc0ebd666e4d0bc7d3a322cda
Leafguyk <81000589+Leafguyk@users.noreply.github.com>
2022-08-21 23:35:16 +0900

    create react app
    - react app created
    - 2048 title created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Title from "./components/title";
+function App() {
+  return (
+    <div>
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/title.jsx
@@ -0,0 +1,10 @@
+function Title(){
+    return(
+        <div className='container mt-10'>
+            <h1 className='text-center text-2xl text-gray-200'>
+            2048
+            </h1>
+        </div>
+    )
+}
+export default Title;
